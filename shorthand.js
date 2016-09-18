3c68c756ed0248fde1ae9e85b704747377408160
Martin Van Vliet <martin@Riverrun.local>
2016-09-18 23:11:53 +0200

    Made shorthand use redis for storage

--- shorthand.js
+++ shorthand.js
@@ -1,8 +1,19 @@
+// TODO: implement usage of redis & attempt to run CD pipeline using docker-compose
+// NOTE: XLR 4.6 won't run the docker-compose plugin
 
-//Lets require/import the HTTP module
+// require modules
 var http = require('http');
 var dispatcher = require('httpdispatcher');
+var redis = require('redis');
+
+// backwards compatibility, can be removed!
 var cache = {};
+
+// redis client
+var client = redis.createClient(6379, 'redis');
+client.on('connect', function() {
+    console.log('Connected to redis');
+});
 
 //Lets define a port we want to listen to
 const PORT=8080; 
@@ -11,7 +22,7 @@ const PORT=8080;
 function handleRequest(request, response){
     try {
         //log the request on console
-        //console.log(request.url);
+        console.log(request.url);
         //Disptach
         dispatcher.dispatch(request, response);
     } catch(err) {
@@ -32,29 +43,40 @@ function generateId() {
 //A sample GET request    
 dispatcher.onGet(/key\/([^\/]+)$/, function(req, res) {
     var key = req.url.match(/key\/([^\/]+)$/)[1];
-    var value = cache[key];
     var result = "";
-    if (value == null) {
-        res.writeHead(404, {'Content-Type': 'application/json'});
-        result = { "error": "key not found" }
-    } else {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        result = { "value": value }
-    }
-    res.write(JSON.stringify(result))
-    res.end();
+
+    client.exists(key, function(err, reply) {
+        if (reply === 1) {
+            console.log('key ' + key + ' found');
+
+            client.get(key, function(err, reply) {
+                console.log('reply = ' + reply);
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                result = { "value": reply }
+                res.write(JSON.stringify(result))
+                res.end();
+            });
+        } else {
+            console.log('key ' + key + ' NOT found');
+
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            result = { "error": "key not found" }
+            res.write(JSON.stringify(result))
+            res.end();
+        }
+    });
 });    
 
 //A sample POST request
 dispatcher.onPost(/val\/([^\/]+)$/, function(req, res) {
     var val = req.url.match(/val\/([^\/]+)$/)[1];
     var key = generateId();
-    cache[key] = val
-
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    var result = { "key": key }
-    res.write(JSON.stringify(result))
-    res.end();
+    client.set(key, val, function(err, reply) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        var result = { "key": key }
+        res.write(JSON.stringify(result))
+        res.end();
+    });
 });
 
 //Create a server
